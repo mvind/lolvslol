bbc7609d4c054a99d042a63be3bc0b37e1d5f948
mvind <vms754@protonmail.com>
2018-09-03 22:15:21 +0200

    api response is complete

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request
-
+from matchup import matchup, convert
 
 @app.route('/')
 @app.route('/index')
@@ -11,8 +11,13 @@ def index():
 @app.route('/result', methods=['POST','GET'])
 def result():
     if request.method == 'POST':
-        res = request.form
-        print(dict(res))
-        return render_template('result.html', data = res)
+        post_data = request.form
+
+        form_res = {}
+        form_res['Name'] = post_data['Name']
+        form_res['Roles'] = post_data['Roles']
+
+        api_res = matchup(form_res)
+        return render_template('result.html', data = api_res)
 
     return render_template('result.html')
--- matchup.py
+++ matchup.py
@@ -2,7 +2,8 @@ import requests, json, id
 from key import api_key
 
 base_urL = 'http://api.champion.gg/champion?'
-
+#/champions/:id/:role/matchups
+match_url = 'http://api.champion.gg/v2/champions/'
 
 # r = requests.get(url)
 # url2 = 'http://api.champion.gg/champion/draven?api_key='+api_key
@@ -13,19 +14,6 @@ base_urL = 'http://api.champion.gg/champion?'
 # print(r2.json())
 # #print(r.json())
 # #/champion/:name?api_key=<API_KEY>
-
-test_data = {'Name': ['Draven'], 'Roles': ['adc']}
-
-def matchup(res):
-    '''return matchup data from  user response
-
-    Args:
-        res: {'Name': ['Draven'], 'Roles': ['adc']}
-
-    returns: whatever
-    '''
-    
-
 
 
 def convert(d):
@@ -42,12 +30,28 @@ def convert(d):
     if type(d) == int:
         for c in id.li:
             if c['id'] == d:
-                return c['name'], 200
+                return str(c['name'])
         return 'No ID match', 0
     elif type(d) == str:
         for c in id.li:
             if c['name'] == d:
-                return c['id'], 200
+                return str(c['id'])
         return 'No name match', 0
     else:
         return 'Nothing found', 0
+
+
+test_data = {'Name': 'Draven', 'Roles': 'DUO_CARRY'}
+
+def matchup(res):
+    '''return matchup data from  user response
+
+    Args:
+        res: {'Name': ['Draven'], 'Roles': ['adc']}
+
+    returns: whatever
+    '''
+    url = match_url + str(convert(res['Name'])) + '/' + str(res['Roles']) + '/matchups?api_key=' + api_key
+    r = requests.get(url)
+    r = r.json()
+    return r
